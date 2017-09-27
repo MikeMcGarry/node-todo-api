43ee223e33d3cacbd22e5e613beb4bf839127cf5
Mike McGarry <mikemcgarry@live.com.au>
2017-09-27 17:43:23 +1000

    Set up express

--- server/server.js
+++ server/server.js
@@ -1,13 +1,30 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const port = global.PORT | 3000
+
 var {mongoose} = require('./db/mongoose')
 var {Todo} = require('./models/todo')
 var {User} = require('./models/user')
 
 var app = express()
 
-app.post('/', (req, res) => {
-  console.log(req)
-  res.send("heyyyyyyy")
+app.use(bodyParser.json())
+
+app.post('/todos', (req, res) => {
+  console.log(req.body)
+  var todo = new Todo({
+    "text": req.body.text
+  })
+  todo.save().then((doc) => {
+    console.log('Note saved')
+    res.send(`Note Saved ${doc}`)
+  }, (err) => {
+    console.log('Error saving note')
+    res.status(400).send(`${err}`)
+  })
+
 })
 
-app.listen(3000)
+app.listen(port, () => {
+  console.log(`started on port ${port}`)
+})
